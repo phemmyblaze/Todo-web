a279d8ecca2a09d5c071daf52a6c3f6f643c72b7
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2023-02-20 18:41:09 +0100

    Functionalities added

---
+++ Js/script.js
@@ -0,0 +1,7 @@
+const form = document.querySelector("#new-form-task")
+const input = document.querySelector("#new-task-input")
+const list_el = document.querySelector("#tasks")
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+})
