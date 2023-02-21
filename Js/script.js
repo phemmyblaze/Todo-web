bbc34832d8fe55587af0f6f94ae8b42b536326ca
Akinuliola Akinbobola Oluwafemi <akinuliola_a@yahoo.com>
2023-02-21 15:10:53 +0100

    code refactoring

--- Js/script.js
+++ Js/script.js
@@ -1,7 +1,34 @@
 const form = document.querySelector("#new-form-task")
-const input = document.querySelector("#new-task-input")
 const list_el = document.querySelector("#tasks")
-
+let todos = JSON.parse(localStorage.getItem("todos"));
+let arr = []
 form.addEventListener("submit", (e) => {
+    const input = document.querySelector("#new-task-input").value
     e.preventDefault();
-})
+    if(input.length == 0){
+        // alert("Please enter a task")
+        console.log("okay");
+    }else{
+        let data = {
+            task : input
+        }
+        arr.push(data)
+        localStorage.setItem("task", JSON.stringify(arr))
+
+    }
+});
+
+// function render(){
+
+// }
+// render()
+
+// function del(){
+
+// }
+
+
+
+
+
+
